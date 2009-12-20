e1817a13b562d536a1d187cd4d31cc5482a8e845
isit <webmaster@isitchristmas.com>
2009-12-20 10:51:40 -0700

    Updated No in Chinese

--- iic.js
+++ iic.js
@@ -130,7 +130,7 @@ function no(countryCode) {
     "AR": "NO", // Argentina
     "EE": "EI", // Estonia
     "HR": "NE", // Croatia
-    "CN": "BÙ SHÌ", // China (Mandarin)
+    "CN": "BÚ SHÌ", // China (Mandarin)
     "IN": "NAHIM", // India
     "SG": "TIDAK", // Singapore
     "PH": "HINDI", // Phillipines
