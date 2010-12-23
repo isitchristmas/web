eb50917c1baaf285be812795d42008609170a11c
Eric Mill <webmaster@isitchristmas.com>
2010-12-22 16:41:23 -0800

    Converted to HTML5

--- index.php
+++ index.php
@@ -6,24 +6,26 @@ $answer = isItChristmas();
 $countryCode = getCountryCode();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en">
 <head>
+  <meta charset="utf-8" />
+  
   <title>Is it Christmas?</title>
+  
   <link rel="alternate" title="Is It Christmas?" href="rss.xml" type="application/rss+xml" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <script type="text/javascript" src="iic.js"></script>
 </head>
 
 <body style="text-align: center; padding-top: 200px;">
 
-  <a href="http://github.com/isit/christmas" 
+  <a href="rss.xml" 
     style="font-weight: bold; 
           font-size: 120pt; 
           font-family: Arial, sans-serif; 
           text-decoration: none; 
           color: black;" 
-    title="CODE"
+    title="RSS"
     id="answer">
     <noscript><?= $answer ?></noscript>
   </a>
@@ -34,14 +36,18 @@ $countryCode = getCountryCode();
   
 
   <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-252618-5");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-252618-5']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 
 </body>
 </html>
