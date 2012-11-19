e987908d31098a10508ea6e4e47684b3e4b8aca6
Eric Mill <konklone@gmail.com>
2012-11-18 23:43:38 -0500

    mocked up RSS view ready with controller

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-/* main route */
-
 var index = function(req, res) {
   res.render('index');
 };
 
+var rss = function(req, res) {
+  res.set({'Content-Type': 'application/rss+xml'});
+  res.render('rss.xml');
+};
 
+
+/** server setup **/
 
 var express = require('express')
   , http = require('http')
@@ -14,8 +18,8 @@ var app = express();
 
 app.configure(function(){
   app.engine('.html', require('ejs').__express);
-  app.set('views', __dirname);
-  app.set('port', process.env.PORT || 3000);
+  app.engine('.xml', require('ejs').__express);
+  app.set('port', process.env.PORT || 80);
   app.set('view engine', 'html');
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
   app.use(express.static(path.join(__dirname, 'public')));
@@ -26,6 +30,7 @@ app.configure('development', function(){
 });
 
 app.get('/', index);
+app.get('/rss.xml', rss);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
