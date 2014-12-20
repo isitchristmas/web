25cf7d24df45a11fb30c75fcc65a5821387228a6
RnbwNoise <rnbwnoise@yandex.ru>
2014-12-20 16:50:31 -0500

    API: Added support for user-defined event listeners.

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,70 @@
 var IIC = {
+    userListeners = {};
+    
+    addEventListener: function(event, listener) {
+        // We aren't calling user listeners for this event: start doing so.
+        if(!this.userListeners[event]) {
+            this.userListeners[event] = [];
+            
+            var defaultListener = events[event];
+            events[event] = function(data) {
+                // Call the system's listener.
+                defaultListener(data);
+                
+                // And then all of the user-defined ones.
+                for(var i = 0; i < this.userListeners[event].length; i++) {
+                    if(this.userListeners[event][i])
+                        this.userListeners[event][i](data);
+                }
+            }.bind(this);
+        }
+        
+        // Add user's listener.
+        return [ event, this.userListeners[event].push(listener) - 1 ];
+    },
+    
+    removeEventListener: function(listenerId) {
+        // Delete the listener.
+        this.userListeners[listenerId[0]][listenerId[1]] = null;
+    },
+    
+    
+    onChat: function(listener) {
+        return this.addEventListener('chat', function(data) { listener(data.id, data.message); });
+    },
+    
+    onMovement: function(listener) {
+        return this.addEventListener('motion', function(data) { listener(data.id, data.x, data.y); });
+    },
+    
+    onRotation: function(listener) {
+        return this.addEventListener('scroll', function(data) { listener(data.id, data.angle / 180 * Math.PI); });
+    },
+    
+    onWave: function(listener) {
+        return this.addEventListener('click', function(data) {
+            if(data.button === 'left')
+                listener(data.id, data.x, data.y);
+        });
+    },
+    
+    onGhost: function(listener) {
+        return this.addEventListener('click', function(data) {
+            if(data.button === 'right')
+                listener(data.id, data.x, data.y);
+        });
+    },
+    
+    
+    isConnected: function(userId) {
+        return !!others[userId];
+    },
+    
+    
+    getCountry: function(userId) {
+        return userId ? others[userId].country : me.country;
+    },
+    
     setCountry: function(countryCode) {
         me.country = countryCode;
         
